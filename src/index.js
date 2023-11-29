1a11506773502539242b9689c512470dd5aae078
203422 <203422@ids.upchiapas.edu.mx>
2023-11-28 20:23:56 -0600

    Server raspberry

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { SerialPort, ReadlineParser } = require('serialport');
+const ioClient = require('socket.io-client');
+
+const port = process.env.PORT || 5000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const socketPrincipal = ioClient(process.env.API_URL);
+
+io.on('connection', (socket) => {
+    console.log('Usuario conectado');
+});
+
+server.listen(port, () => {
+    console.log('Servidor ejecutándose en el puerto ', port);
+});
+
+const getAndSubmitData = () => {
+
+    const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 9600 })
+    const parser = new ReadlineParser()
+
+    port.pipe(parser);
+
+    parser.on('open', function () {
+        console.log('connection is opened');
+    });
+
+    parser.on('data', (data) => {
+        // Enviar datos a la API
+        socketPrincipal.emit('data-to-api', data);
+    })
+
+
+
+
+    // Emitir a clientes conectados si es necesario
+    io.emit('data-clients', data);
+};
+
+// simulateSensorData();
+
+// socketPrincipal.on('enviar-datos', (data) => {
+//     console.log('Recibido desde el servidor principal local:', data);
+// });
+
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// function getRandomChoice(choices) {
+//     const randomIndex = Math.floor(Math.random() * choices.length);
+//     return choices[randomIndex];
+// }
+
+// function simulateSensorData() {
+//     const nivelAgua = getRandomArbitrary(0, 200);
+//     const temperaturaAgua = getRandomArbitrary(20, 30);
+//     const lluvia = 9600;
+//     const ph = getRandomArbitrary(6.0, 8.0);
+//     const actuadorCloro = getRandomChoice(["Activo", "Inactivo"]);
+
+//     getAndSubmitData({ nivelAgua, temperaturaAgua, lluvia, ph });
+// }
+
+// setInterval(simulateSensorData, 5000);
+
